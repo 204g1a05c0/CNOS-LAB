1c1783612b5224b856ed373bf50c0873f71b3a04
varun <112166017+204g1a05c0@users.noreply.github.com>
2022-09-16 15:00:02 +0530

    created a fcfs.c file successfully

--- OSLAB/EXp-1/FCFS/FCFS.c
+++ OSLAB/EXp-1/FCFS/FCFS.c
@@ -1,4 +1,9 @@
 /*Name of the student : U.varun
 Roll number : 204g1a05c0
 name of the experiment : First Come First Serve
-experiment schedule : 16-09-2022 */
+experiment schedule : 16-09-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
