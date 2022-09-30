66d997df98ababca32e655fbe93b5b3c01f2e96c
varun <112166017+204g1a05c0@users.noreply.github.com>
2022-09-30 14:58:19 +0530

    update FCFS.c

--- OSLAB/EXp-1/FCFS/FCFS.c
+++ OSLAB/EXp-1/FCFS/FCFS.c
@@ -4,12 +4,11 @@ name of the experiment : First Come First Serve
 experiment schedule : 16-09-2022
 */
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
+
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -32,5 +31,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
 }
